3e7806002e3b0414a40f1f98731e6878daa7994c
vrtulka23 <vrtulka23@pm.me>
2024-09-13 20:15:46 +0200

    added exs project

---
+++ exs.rb
@@ -0,0 +1,19 @@
+class Exs < Formula
+  desc "Implementation of expression solver in C++"
+  homepage "https://github.com/vrtulka23/exs-cpp"
+  url "https://github.com/vrtulka23/exs-cpp/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "f86d21614ff9ff2d89cd729060fca789be8bcadf83b26ba36c3941db2c93a33b"
+  license "MIT"
+
+  depends_on "cmake" => :build
+  depends_on "googletest"
+  
+  def install
+    system "sh", "setup.sh", "-b"
+    include.install Dir["src/*"]
+  end
+
+  test do
+    system "false"
+  end
+end
