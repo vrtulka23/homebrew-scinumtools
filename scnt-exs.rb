f0cdfa96864b29c38da0d18a084e3d208d7545c4
vrtulka23 <vrtulka23@pm.me>
2024-09-17 22:50:19 +0200

    updated exs formula

--- scnt-exs.rb
+++ scnt-exs.rb
@@ -1,8 +1,8 @@
 class ScntExs < Formula
   desc "Implementation of expression solver in C++"
   homepage "https://github.com/vrtulka23/scnt-exs"
-  url "https://github.com/vrtulka23/scnt-exs/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "f86d21614ff9ff2d89cd729060fca789be8bcadf83b26ba36c3941db2c93a33b"
+  url "https://github.com/vrtulka23/scnt-exs/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "f95384d4b30a3ad8747255dc9cab0aae593fe595a92a4b7340d054a5e730f5db"
   license "MIT"
 
   depends_on "cmake" => :build
