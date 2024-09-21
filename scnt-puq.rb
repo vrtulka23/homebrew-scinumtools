01bf553b763f1689420f258bdca3cd30d37aa825
vrtulka23 <vrtulka23@pm.me>
2024-09-21 14:44:19 +0200

    updated version

--- scnt-puq.rb
+++ scnt-puq.rb
@@ -1,8 +1,8 @@
 class ScntPuq < Formula
   desc "Implementation of physical unit calculations in C++"
   homepage "https://github.com/vrtulka23/scnt-puq"
-  url "https://github.com/vrtulka23/scnt-puq/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "ec76598a98e11b3cf2dc09cf714e8605aaeee1e6c01366d3ead5360dcf3381f6"
+  url "https://github.com/vrtulka23/scnt-puq/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "233414591e3c9cb9ecbe5c3083a6b4069e68635e3f62db40835ce24aeede6cc7"
   license "MIT"
 
   depends_on "cmake" => :build
