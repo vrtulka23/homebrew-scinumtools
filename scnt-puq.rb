50d0a007bd46c8ae325cb054e4a5b1e883c463e3
vrtulka23 <vrtulka23@pm.me>
2024-10-07 23:41:47 +0200

    updated version

--- scnt-puq.rb
+++ scnt-puq.rb
@@ -1,18 +1,19 @@
 class ScntPuq < Formula
   desc "Implementation of physical unit calculations in C++"
   homepage "https://github.com/vrtulka23/scnt-puq"
-  url "https://github.com/vrtulka23/scnt-puq/archive/refs/tags/v1.1.4.tar.gz"
-  sha256 "fa4364786d4e16c318523de5ef6fdf138627509cad17616125ccdb1f506e6b6a"
+  url "https://github.com/vrtulka23/scnt-puq/archive/refs/tags/v1.3.1.tar.gz"
+  sha256 "f7456c0d9727ef65745d0d7277b4366afa63ff02f45c1a9468126661a48a8a19"
   license "MIT"
 
   depends_on "cmake" => :build
   depends_on "googletest"
   depends_on "google-benchmark"
   depends_on "vrtulka23/scinumtools/scnt-exs"
+  depends_on "python@3.y"
   
   def install
-    system "sh", "setup.sh", "-b"
-    bin.install "build/puq"
+    system "cmake", "..", *std_cmake_args
+    system "make", "install"
   end
 
   test do
