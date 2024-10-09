70adcbf8ecb305f8aa0346349d636a8f191c8a73
vrtulka23 <vrtulka23@pm.me>
2024-10-09 10:23:50 +0200

    upated installer

--- scnt-puq.rb
+++ scnt-puq.rb
@@ -9,27 +9,22 @@ class ScntPuq < Formula
   depends_on "googletest"
   depends_on "google-benchmark"
   depends_on "vrtulka23/scinumtools/scnt-exs"
-  depends_on "python@3.12"
-  
-  def pythons
-    deps.map(&:to_formula)
-      .select { |f| f.name.start_with?("python@") }
-      .sort_by(&:version) # so scripts like `bin/f2py` use newest python
-  end
+  depends_on "python@3.x"
+  depends_on "pybind11"
   
   def install
+    # install scnt-puq
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
       system "make", "install"
     end
-    pythons.each do |python|
-      python3 = python.opt_libexec/"bin/python"
-      system python3, "-m", "pip", "install", "pypuq",
-             *std_pip_args(build_isolation: true)
-    end
+    # install pypuq
+    ENV.prepend_path "PATH", Formula["python@3.x"].opt_bin 
+    system "pip3", "install", "pypuq>=1.0" 
   end
 
   test do
-    system "puq", "-v"
+    system "#{bin}/puq", "-v"
+    system "python3", "-c", "import pypuq; print(pypuq.__version__)"
   end
 end
